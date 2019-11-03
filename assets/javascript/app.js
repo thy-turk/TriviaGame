77b80916d105d2d3716378eb4cbdcf0777a5bb3b
Turk <tuckerfiumara@gmail.com>
2019-11-02 19:49:47 -0700

    Started css work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,67 @@
 var correct = 0;
-counter = 90;
+var counter = 90;
+var downloadTimer;
 
 var questions = [
     {
-        question: "Choose 3",
+        question: "How old do researchers believe the world is?",
 
         answers: [
-             '1',
-             '2',
-             '3',
-             '4',
+             '13 million years',
+             '2.7 billion years',
+             '13.8 billion years',
+             '1.3 trillion years',
 
         ],
-        correctAnswer: '3'
+        correctAnswer: '13.8 billion years'
     },
     {
-        question: "Choose 4",
+        question: "How many Earths can fit in the sun?",
 
         answers: [
-             '1',
-             '2',
-             '3',
-             '4',
+             '1,000',
+             '150,000',
+             '643,000',
+             '1,300,000',
 
         ],
-        correctAnswer: '4'
+        correctAnswer: '1,300,000'
     },
     {
-        question: "Choose 1",
+        question: "In what year did the US first land on the moon?",
 
         answers: [
-             '1',
-             '2',
-             '3',
-             '4',
+             '1964',
+             '1967',
+             '1969',
+             '1973',
 
         ],
-        correctAnswer: '1'
+        correctAnswer: '1969'
+    },
+    {
+        question: "Which country was the first to launch a satellite into space?",
+
+        answers: [
+             'United States',
+             'China',
+             'Russia',
+             'India',
+
+        ],
+        correctAnswer: 'Russia'
+    },
+    {
+        question: "What was the first animal sent into space?",
+
+        answers: [
+             'A dog named Laika',
+             'A cat named Felicette',
+             'A tortiose named George',
+             'A monkey called Albert',
+
+        ],
+        correctAnswer: 'A dog named Laika'
     },
     
 ];
@@ -47,8 +72,7 @@ function createquestions() {
     for (var i = 0; i < questions.length; i++) {
         $('#quiz').append("<h3>" + questions[i].question + "<h3>");
         for (var j = 0; j < questions[i].answers.length; j++) {
-            $('#quiz').append("<div class='checked'><input type='radio' name='question-" + i +
-            "' value='" + questions[i].answers[j] + "''>" + "<span class='word'>" + questions[i].answers[j] + "</span></div>");
+            $('#quiz').append("<div class='checked'><input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "''>" + "<span class='word'>" + questions[i].answers[j] + "</span></div>");
         }
     }
 }
@@ -66,7 +90,10 @@ function submit() {
     $('#results').show();
     $('#quiz').hide();
     $('#submit').hide();
-    $('#results').html("You got " + correct + "/3 questions correct!")
+    $('#timer').hide();
+    $('#results').html("You got " + correct + "/5 questions correct!")
+    clearInterval(downloadTimer);
+
 
 }
 
@@ -83,14 +110,25 @@ function quizSubmit() {
   }
 }
 
-
-
 function timerStart() {
-    setInterval(counter--, 1000);
-    $('#timer').html(counter);
-
+var downloadTimer = setInterval(function(){
+  $('#timer').html(counter + " seconds remaining");
+  counter -= 1;
+  if(counter <= -1){
+    clearInterval(downloadTimer);
+    outOfTime();
+  } 
+}, 1000);
 }
 
+function outOfTime() {
+    $('#results').show();
+    $('#quiz').hide();
+    $('#submit').hide();
+    $('#timer').hide();
+    $('#results').html("You ran out of time! You got " + correct + "/5 questions correct!")
+    
+}
 
 createquestions();
 
@@ -101,10 +139,5 @@ $('#timer').hide();
 
 $('#startBtn').on("click", startGame);
 
-
-
-
-
- 
 $('#submit').on("click", submit);
 
